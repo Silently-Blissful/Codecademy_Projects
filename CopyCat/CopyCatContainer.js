1265782e4506200896aaa24d023cb1f1ff430400
Silently-Blissful <93438995+Silently-Blissful@users.noreply.github.com>
2022-07-15 20:03:02 -0600

    Create CopyCatContainer.js

---
+++ CopyCat/CopyCatContainer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import CopyCat from '../components/CopyCat.js'
+
+export class CopyCatContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copying: true,
+      input: ''
+    };
+
+    this.toggleTape = this.toggleTape.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleTape() {
+    this.setState({ copying: !this.state.copying })
+  }
+
+  handleChange(e){
+    console.log(e.target);
+    this.setState({input: e.target.value})
+  }
+
+  render() {
+    return (
+           
+      <CopyCat 
+      copying={this.state.copying}
+      toggleTape={this.toggleTape}
+      handleChange={this.handleChange}
+      input={this.state.input}
+       />
+
+    );
+  };
+}
+
+ReactDOM.render(<CopyCatContainer />, 
+  document.getElementById('app'));
